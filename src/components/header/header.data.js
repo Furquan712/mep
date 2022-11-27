57881bf64668316e5a55392a91b7ef4365edf6bc
Furquan712 <smfurquan7.12@gmail.com>
2022-11-27 14:28:46 +0530

    mep project

---
+++ src/components/header/header.data.js
@@ -0,0 +1,29 @@
+export default [
+  {
+    path: '/',
+    label: 'Home',
+  },
+  {
+    path: '/about',
+    label: 'About',
+  },
+  {
+    path: '/projects',
+    label: 'Project',
+  },
+  {
+    path: 'feature',
+    label: 'Services',
+  },
+  {
+    path: 'blog',
+    label: 'Blog',
+  },
+  
+  {
+    path: '/contact',
+    label: 'Contact',
+  },
+ 
+   
+];
